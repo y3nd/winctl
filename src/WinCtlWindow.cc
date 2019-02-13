818ca73153a0aacabb443a63a5130b0c974da465
Yoann N <y3ndev@gmail.com>
2019-02-13 20:53:01 +0100

    Update WinCtlWindow.cc

--- src/WinCtlWindow.cc
+++ src/WinCtlWindow.cc
@@ -47,7 +47,7 @@ NAN_METHOD(Window::New) {
 		const int argc = 1;
 		v8::Local<v8::Value> argv[argc] = {info[0]};
 		v8::Local<v8::Function> cons = Nan::New(constructor);
-		info.GetReturnValue().Set(cons->NewInstance(argc, argv));
+		info.GetReturnValue().Set(cons->NewInstance(argv));
 	}
 }
 
